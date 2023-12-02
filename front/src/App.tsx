4b375bdd2152c7255e7c34675c7da7d755385d1e
recodeyo1ko <kuronekoryohei@gmail.com>
2023-12-02 20:17:10 +0900

    Revert "Merge pull request #3 from recodeyo1ko/create-post"
    This reverts commit 501de5889d17e15e19f55b230157392db10f38f6, reversing
    changes made to b4d1b1491e9ea7f084cbdd0366fa96f56d3da7d2.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
-import "./App.css";
-import Form from "./components/Form";
-
-interface Book {
-  title: string;
-  body: string;
-}
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [book, setBook] = useState<Book>({ title: "", body: "" });
   return (
     <div className="App">
-      <Form book={book} setBook={setBook} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
